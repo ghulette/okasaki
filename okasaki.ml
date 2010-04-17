ac85c7aec8fdad7bdd5d40e6c1132b9babd8129e
Geoff Hulette <ghulette@cs.uoregon.edu>
2010-04-17 14:23:20 -0700

    Added binomial heaps

--- okasaki.ml
+++ okasaki.ml
@@ -1,5 +1,6 @@
 exception Empty
 exception Not_found
+exception Undefined
 
 module type ORDERED =
 sig
@@ -34,7 +35,12 @@ struct
   type set = tree
   
   let empty = E
-  
+
+  (* Complexity for unbalanced binary trees depends on the depth (d) of the
+  binary tree.  At best, the  depth is log(n), but in the worst case it 
+  could be n.*)
+
+  (* member: O(d) *)
   let rec member x = function
     | E -> false
     | T (a,y,b) ->
@@ -42,13 +48,15 @@ struct
       else if (Elem.eq x y) then true
       else member x b
     
+  (* insert: O(d) *)
   let rec insert x = function
     | E -> T (E,x,E)
     | T (a,y,b) as r->
       if (Elem.lt x y) then T (insert x a,y,b)
       else if (Elem.eq x y) then r
       else T (a,y,insert x b)
-      
+
+  (* members: n *)
   let rec members = function
     | E -> []
     | T (a,x,b) -> (members a) @ [x] @ (members b)
@@ -83,20 +91,74 @@ struct
     if rank a >= rank b then T (rank b + 1,x,a,b)
     else T (rank a + 1,x,b,a)
 
+  (* merge: O(log n) *)
   let rec merge h1 h2 = match h1,h2 with
     | h,E -> h
     | E,h -> h
     | T (_,x,a1,b1),T (_,y,a2,b2) -> 
       if (Elem.leq x y) then make x a1 (merge b1 h2)
       else make y a2 (merge h1 b2)
-      
+
+  (* insert: O(log n) *)
   let insert x h = merge (T (1,x,E,E)) h
   
+  (* find_min: O(1) *)
   let find_min = function
     | E -> raise Empty
     | T (_,x,_,_) -> x
   
+  (* delete_min: O(log n) *)
   let delete_min = function
     | E -> raise Empty
     | T (_,x,lh,rh) -> merge lh rh  
+end
+
+module BinomialHeap : HEAP = functor (Elem : ORDERED) ->
+struct
+  type tree = Node of int * Elem.t * tree list
+  let rank (Node (r,_,_)) = r
+  let root (Node (_,x,_)) = x
+  
+  type heap = tree list
+  
+  let empty = []
+  
+  let is_empty = function
+    | [] -> true
+    | _ -> false
+    
+  let link (Node (r,x,c1) as t1) (Node (_,y,c2) as t2) =
+    if Elem.leq x y then Node (r+1,x,t2::c1)
+    else Node (r+1,y,t1::c2)
+    
+  let rec insert_tree t = function
+    | [] -> [t]
+    | t'::ts' as ts ->
+      if rank t < rank t' then t :: ts 
+      else insert_tree (link t t') ts'
+    
+  let insert x ts = insert_tree (Node (0,x,[])) ts
+    
+  let rec merge ts1 ts2 = match ts1,ts2 with
+    | ts,[] -> ts
+    | [],ts -> ts
+    | t1::ts1',t2::ts2' ->
+      if rank t1 < rank t2 then t1 :: merge ts1' ts2
+      else if rank t2 < rank t1 then t2 :: merge ts1 ts2'
+      else insert_tree (link t1 t2) (merge ts1' ts2')
+      
+  let rec remove_min_tree = function
+    | [] -> raise Empty
+    | [t] -> (t,[])
+    | t::ts -> 
+      let (t',ts') = remove_min_tree ts in
+      if Elem.leq (root t) (root t') then (t,ts) else (t',t::ts')
+  
+  let find_min ts = 
+    let (t,_) = remove_min_tree ts in 
+    root t
+    
+  let delete_min ts = 
+    let (Node (_,x,ts1),ts2) = remove_min_tree ts in
+    merge (List.rev ts1) ts2
 end
--- test.ml
+++ test.ml
@@ -2,24 +2,29 @@ open Okasaki
 
 module IntSet = UnbalancedSet(Integer)
 open IntSet
-
 let test_set () =
   let root = insert 5 (insert 10 (insert 0 (insert 1 empty))) in
   let elts = members root in
   let str = String.concat "," (List.map string_of_int elts) in
   Printf.printf "Set: %s\n" str
 
-
-
-module IntHeap = LeftistHeap(Integer)
-open IntHeap
-  
-let test_heap () =
+module IntLeftHeap = LeftistHeap(Integer)
+open IntLeftHeap
+let test_leftheap () =
   let root = insert 5 (insert 10 (insert 0 (insert 7 empty))) in
   let min1 = find_min root in
   let min2 = find_min (delete_min root) in
-  Printf.printf "Heap: %d,%d\n" min1 min2
-  
+  Printf.printf "LeftHeap: %d,%d\n" min1 min2
+
+module IntBinHeap = BinomialHeap(Integer)
+open IntBinHeap
+let test_binheap () =
+  let root = insert 5 (insert 10 (insert 0 (insert 7 empty))) in
+  let min1 = find_min root in
+  let min2 = find_min (delete_min root) in
+  Printf.printf "BinHeap: %d,%d\n" min1 min2
+
 ;;
 test_set();
-test_heap();
+test_leftheap();
+test_binheap();
